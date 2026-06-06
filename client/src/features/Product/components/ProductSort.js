e6aac3f69b1ca16406f199b3ef552f3c182438e3
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-07 05:20:06 +0530

    Add Filter Components

--- client/src/features/Product/components/ProductSort.js
+++ client/src/features/Product/components/ProductSort.js
@@ -1,3 +1,6 @@
+//add sort button
+
+
 function SortButton() {
   return (
     <button className="group flex items-center gap-2 text-[13px] uppercase tracking-[3px] text-black/70 hover:text-black transition-all">
