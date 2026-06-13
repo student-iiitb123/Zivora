322cf020565fec04dd232b3ea5efc2d9aec3f453
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-13 11:50:37 +0530

    Add auth validation

--- server/src/modules/auth/auth.routes.js
+++ server/src/modules/auth/auth.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { signup, login } from "./auth.controller.js";
-
+import { signupValidation } from "./auth.validation.js";
+import { loginValidation } from "./auth.validation.js";
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/signup", signupValidation, signup);
+router.post("/login", loginValidation, login);
+
+
 
 export default router;
