a2f8487963b868e4c182f80da70143d4a29f1b68
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-13 10:27:24 +0530

    setup mongoDb and server

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,14 @@
-import express from 'express';
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
+
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+connectDB();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is on ${PORT}`);
+});
