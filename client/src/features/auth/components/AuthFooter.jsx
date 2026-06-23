ac214163c1ba6fc0794a299887f04b680f1b9290
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-23 20:57:25 +0530

    asdfgh

--- client/src/features/auth/components/AuthFooter.jsx
+++ client/src/features/auth/components/AuthFooter.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 function AuthFooter() {
   return (
     <footer className="mt-12 text-center">
       <p className="font-label-md text-secondary">
         Don't have an account?{" "}
-        <a
+        <Link
+          to="/register"
           className="text-primary font-bold hover:underline underline-offset-4 decoration-1"
-          href="/register"
         >
           Register Now
-        </a>
+        </Link>
       </p>
     </footer>
   );
