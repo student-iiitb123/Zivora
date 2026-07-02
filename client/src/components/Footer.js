7b8aaf1ba65e27c8b4f1092e3d78bf79b3fa7c1c
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-07-02 17:47:09 +0530

    change logo

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-12">
           <div className="sm:col-span-2 lg:col-span-1">
              <img
-                src='../../assets/katchy-logo.webp'
+                src='../../assets/katchy-logo.png'
                 alt="Katchy"
                 className="h-20 lg:h-24 w-auto object-contain"
               />
