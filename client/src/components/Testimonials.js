98187e16e754658b3a8335851e80c971dddf2995
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-07-13 11:48:49 +0530

    all done

--- client/src/components/Testimonials.js
+++ client/src/components/Testimonials.js
@@ -7,7 +7,7 @@ import {
   ChevronRight,
 } from "lucide-react";
 
-import { getTopReviews } from "../../../services/reviewService";
+import { getTopReviews } from "../services/reviewService";
 
 export default function Testimonials() {
   const [reviews, setReviews] = useState([]);
